5b89fad21bd0daadd5136dbdabe3b27f352ddcac
Thorongil80 <thomas@herzogonline.net>
2024-01-16 11:15:19 +0100

    initial setup with dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
+use std::fs::File;
+use log::{info, error, warn, LevelFilter};
+use simplelog::{ColorChoice, CombinedLogger, Config, debug, TerminalMode, TermLogger, WriteLogger};
+
 mod imap;
 mod fireplan;
 
+pub struct Configuration {
+    imap_server: String,
+    imap_port: u16,
+    imap_password: String,
+    fireplan_api_key: String
+}
+
+
 
 fn main() {
-    println!("Hello, world!");
+
+    let config : Vec<Configuration> = vec![];
+
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
+            WriteLogger::new(LevelFilter::Trace, Config::default(), File::create("fireplan_alarm_imap.log").unwrap()),
+        ]
+    ).unwrap();
+
+    error!("Bright red error");
+    info!("This only appears in the log file");
+    debug!("This level is currently not enabled for any logger");
+
 }
